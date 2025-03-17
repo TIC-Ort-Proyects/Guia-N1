8699fce1cac2d8b781d5e4bf4808954629563724
eitan-que <eitanfeldman021@gmail.com>
2025-03-17 12:14:35 -0300

    Variables Declaradas e Inicializadas

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int studentQuantit; // Numeros enteros
+    float tableLengthInMeters; // Numeros decimales
+    string userAdress; // Lineas de texto
+    bool agreeTermsAndConditions; // Verdadero / Falso
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        studentQuantit = 28;
+        tableLengthInMeters = 1.7f;
+        userAdress = "Libertador 5670";
+        agreeTermsAndConditions = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
